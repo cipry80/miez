40daff1e79b57272c6d0468a20b248343874cb09
Ciprian Vacaru <cvacaru@sdl.com>
2016-06-06 18:16:26 +0300

    up folder tree

---
+++ app/models/productModel.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const mongoose = require('mongoose');
+// your common helpers file
+const commonHelper = require('../helpers/common');
+// custom schema
+const Money = require('./moneyModel').schema;
+const ProductDetail = require('./productDetailModel').schema;
+const Schema = mongoose.Schema;
+// mongoose schema
+const ObjectId = Schema.ObjectId;
+const Mixed = Schema.Types.Mixed;
+
+const ProductSchema = new Schema({
+  sku:          { type: String, required: true },
+  category:     { type: String },
+  slug:         { type: String },
+  images:       { type: [
+    {
+      caption:  { type: String },
+      filename: { type: String }
+    }
+  ] },
+  details:      { type: ProductDetail },
+  price:        { type: Money },
+  active:       { type: Boolean, default: false }
+});
+
+ProductSchema.pre('save', function(next) {
+  // generic `slug` creation method
+  this.slug = commonHelper.createSlug(this.title);
+  next();
+});
+
+ProductSchema.statics.findBySKU = function findBySKU(sku, callback) {
+  this.findOne({ sku: sku }, callback);
+}
+
+ProductSchema.statics.findBySlug = function findBySlug(sku, callback) {
+  this.findOne({ slug: slug }, callback);
+}
+
+module.exports = mongoose.model('Product', ProductSchema);
