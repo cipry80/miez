151e93777849f240fee5acc5a1529b7d381e7629
Ciprian Vacaru <cvacaru@sdl.com>
2016-06-08 18:17:19 +0300

    test folder

---
+++ tests/fixtures/product.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+	sku: '0123456789',
+	title: 'Pizza',
+	category: 'Food',
+	details: {
+		description: 'Something good',
+		summary: 'Margerita'
+	},
+	price: {
+		currency: 'RON',
+		factor: 100,
+		amount: 599
+	},
+	active: true
+}
