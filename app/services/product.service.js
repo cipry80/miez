c74089c5fb99df9127f891b29820d8c76054c3d1
Ciprian Vacaru <cvacaru@sdl.com>
2016-06-09 08:55:26 +0300

    update

--- app/services/product.service.js
+++ app/services/product.service.js
@@ -10,71 +10,57 @@ Class ProductService {
     //constructor
   }
 
-  addProduct(productData, callback) {
-    Product.create(productData, (err, product) => {
-      if  (err && (11000 === err.code || 11001 === err.code)) {
-        return res.status(400).json({ message: 'Duplicate product' });
-      }
-      console.log(productData);
-
-      if  (err) {
-        return callback(err);
-      }
-
-      callback(null, product);
-    });
-  }
-  getProductBySku (sku, callback) {
-
-    Product.findOne({sku: sku}, (err, product) => {
-      if (err) {
-        next(err);
-      } else if (product) {
-        callback(null, product);
-        next();
-      } else {
-        next(new Error('failed to find product'));
-      }
-    });
-  }
-
-  updateProduct (sku, productData, callback) {
-    productData.save((err, updatedProduct) => {
-      if (err) {
-        return callback(err);
-      }
-      callback(null, updatedProduct);
-    });
-  }
-
-  getAllProducts(query, limit,  skip, callback) {
-    if(typeof query === 'function') {
-      callback = query;
-      query = {};
-      limit = MAXLIMIT;
-      skip = 0;
+  addProduct(data, callback) {
+    Product.create(data, callback};
     }
-    if(typeof limit === 'function') {
-      callback = limit;
-      query = {};
-      limit = MAXLIMIT;
-      skip = 0;
-    }
-    if(limit > MAXLIMIT) {
-      limit = MAXLIMIT;
-    }
-    Product.find(query).skip(skip).limit().exec(callback);
-  }
 
-  deleteProduct: function (sku, callback) {
-    ?.remove( err => {
-      if (err) {
-        return next(err);
+    getProductBySku (sku, callback) {
+      // .getProductBySku(sku, function(err, product) { ... });
+      Product.findOne({sku: sku}, callback);
+    }
+
+    updateProduct (sku, data, callback) {
+      // find by sku
+      if(!sku) {
+        let noSku = new Error ('Sku is required');
+        noSku.type = "Sku_required";
+        return callback(noSku);
       }
 
-      res.status(204).json();
-    });
-  }
-}
+      this.findProductBySKU(sku, (err, product) => {
+        if (err) throw err;
+        _.assign(data, callback);
+        console.log(data);
+        product.save(data, callback);
+      });
+    }
+    getAllProducts(query, limit,  skip, callback) {
+      if(typeof query === 'function') {
+        callback = query;
+        query = {};
+        limit = MAXLIMIT;
+        skip = 0;
+      }
+      if(typeof limit === 'function') {
+        callback = limit;
+        query = {};
+        limit = MAXLIMIT;
+        skip = 0;
+      }
+      if(limit > MAXLIMIT) {
+        limit = MAXLIMIT;
+      }
+      Product.find(query).skip(skip).limit().exec(callback);
+    }
 
-module.exports=ProductService;
+    deleteProduct: function (sku, callback) {
+      // find product by sku
+      this.findProductBySKU(sku, (err, product) => {
+        if (err) throw err;
+        // remove product
+        product.remove( sku, callback)
+      });
+      
+    }
+
+    module.exports=ProductService;
---
+++ tests/integration/product.test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+
+const productFixture = require('../fixtures/product');
+const ProductService = require('../../app/services/product.service');
+
+describe('Product Service', function(){
+	let mongoose;
+	let productService;
+
+	before(function(done) {
+		mongoose = require('../../config/mongoose').init();
+		productService = new ProductService();
+		done();
+	});
+
+	it('should instantiate a new Product Service', (done) => {
+		should.exist(productService);
+		done();
+	});
+
+	it('should add a new product', (done) => {
+		productService.addProduct(productFixture, (err, product) => {
+			if (err) throw err;
+
+			should.exist(product);
+			product.sku.should.equal(productFixture.sku);
+			product.title.should.equal(productFixture.title);
+			product.slug.should.equal('choco-caramel-minis');
+			product.price.value.should.equal(5.99);
+			done();
+		});
+	});
+
+	// it should not add products with duplicate ids
+
+	it("should find product by sku", (done) => {
+		productService.findProductBySKU(productFixture.sku, (err, product) => {
+			if (err) throw err;
+
+			should.exist(product);
+			product.sku.should.equal(productFixture.sku);
+			done();
+		});
+	});
+
+	it("should update product by sku", (done) => {
+		const newProductTitle = { title: 'Pizza' };
+
+		productService.updateProduct(productFixture.sku, newProductTitle, (err, product) => {
+			if (err) throw err;
+
+			product.sku.should.equal(productFixture.sku);
+			product.title.should.equal(newProductTitle.title);
+			product.slug.should.equal('mister-choc-choco-caramel-minis');
+			done();
+		});
+
+	});
+
+	// it should update sku
+});
